641fb55c0826908140c411310a5010f803969f05
illfriday <willprouty@gmail.com>
2021-08-25 21:27:24 -0700

    add some emojis!

--- src/App.js
+++ src/App.js
@@ -9,7 +9,76 @@ class App extends Component {
         <div className="App">
           <h1>Welcome to our stater page</h1>
           <p>
-            I created this repo to get us started and set up to start working!
+            I created this repo to get us started and set up to start working! <br/>
+            <span role="img" aria-label="milk">🥛</span>
+            La Leche
+            <br/>
+            <span role="img" aria-label="onion">🧅</span>
+            La Cebolla
+            <br/>
+            <span role="img" aria-label="bread">🍞</span>
+            El Pan
+             <br/>
+            <span role="img" aria-label="corn">🌽</span>
+            El Maize
+            <br/>
+            <span role="img" aria-label="cucumber">🥒</span>
+            El Pepino
+            <br/>
+            <span role="img" aria-label="garlic">🧄</span>
+            El Ajo
+            <br/>
+            <span role="img" aria-label="hot pepper">🌶️</span>
+            El Chile
+             <br/>
+            <span role="img" aria-label="red apple">🍎</span>
+            La Manzana Roja
+             <br/>
+            <span role="img" aria-label="green apple">🍏</span>
+            La Manzana Verde
+            <br/>
+            <span role="img" aria-label="mushroom">🍄</span>
+            El Hongo
+            <br/>
+            <span role="img" aria-label="carrot">🥕</span>
+            La Zanahoria
+            <br/>
+            <span role="img" aria-label="cheese">🧀</span>
+            El Queso
+            <br/>
+            <span role="img" aria-label="shrimp">🍤</span>
+            El Camarone
+             <br/>
+            <span role="img" aria-label="tomato">🍅</span>
+            El Tomate
+             <br/>
+            <span role="img" aria-label="strawberry">🍓</span>
+            La Fresa
+             <br/>
+            <span role="img" aria-label="avocado">🥑</span>
+            El Aguacate
+             <br/>
+            <span role="img" aria-label="pineapple">🍍</span>
+            La Piña
+             <br/>
+            <span role="img" aria-label="egg">🥚</span>
+            El Uevo
+             <br/>
+            <span role="img" aria-label="cherry">🍒</span>
+            La Cereza
+            <br/>
+            <span role="img" aria-label="lemon">🍋</span>
+            El Limon
+            <br/>
+            <span role="img" aria-label="oyster">🦪</span>
+            El Ostion
+            El Limon
+            <br/>
+            <span role="img" aria-label="coconut">🥥</span>
+            El Coco
+            
+
+      
           </p>
         </div>
       </Router>
