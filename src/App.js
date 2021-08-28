20143ee95c448bd32ff04ff37d84817f217c28d2
illfriday <willprouty@gmail.com>
2021-08-28 00:14:52 -0700

    git isssue

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import Home from "./pages/homepage/HomePage";
 import Navigation from "./components/navigation/Navigation";
+import Emojis from "./components/Emojis";
 import "./App.scss";
-
 
 class App extends Component {
   render() {
@@ -12,8 +17,9 @@ class App extends Component {
         <div className="App">
           <Navigation />
           <Switch>
-            <Route to="/" component={Home} />
-            <Route to="/" />
+            <Route exact path="/homepage" component={Home} />
+            <Route exact path="/emojis" component={Emojis} />
+            <Redirect to="/homepage" />
           </Switch>
         </div>
       </Router>
--- src/components/Emojis.js
+++ src/components/Emojis.js
@@ -1,122 +1,12 @@
 import React from "react";
+import CurrentAnswer from "../utility/CurrentAnswer";
 
 function Emojis() {
   return (
     <>
-      <h1>This is the first base Emojis list</h1>
-      <p>
-        I created this repo to get us started and set up to start working!{" "}
-        <br />
-        <span role="img" aria-label="milk">
-          🥛
-        </span>
-        La Leche
-        <br />
-        <span role="img" aria-label="onion">
-          🧅
-        </span>
-        La Cebolla
-        <br />
-        <span role="img" aria-label="bread">
-          🍞
-        </span>
-        El Pan
-        <br />
-        <span role="img" aria-label="corn">
-          🌽
-        </span>
-        El Maíz
-        <br />
-        <span role="img" aria-label="cucumber">
-          🥒
-        </span>
-        El Pepino
-        <br />
-        <span role="img" aria-label="garlic">
-          🧄
-        </span>
-        El Ajo
-        <br />
-        <span role="img" aria-label="hot pepper">
-          🌶️
-        </span>
-        El Chile
-        <br />
-        <span role="img" aria-label="red apple">
-          🍎
-        </span>
-        La Manzana Roja
-        <br />
-        <span role="img" aria-label="green apple">
-          🍏
-        </span>
-        La Manzana Verde
-        <br />
-        <span role="img" aria-label="mushroom">
-          🍄
-        </span>
-        El Hongo
-        <br />
-        <span role="img" aria-label="carrot">
-          🥕
-        </span>
-        La Zanahoria
-        <br />
-        <span role="img" aria-label="cheese">
-          🧀
-        </span>
-        El Queso
-        <br />
-        <span role="img" aria-label="shrimp">
-          🍤
-        </span>
-        El Camaron
-        <br />
-        <span role="img" aria-label="tomato">
-          🍅
-        </span>
-        El Tomate
-        <br />
-        <span role="img" aria-label="strawberry">
-          🍓
-        </span>
-        La Fresa
-        <br />
-        <span role="img" aria-label="avocado">
-          🥑
-        </span>
-        El Aguacate
-        <br />
-        <span role="img" aria-label="pineapple">
-          🍍
-        </span>
-        La Piña
-        <br />
-        <span role="img" aria-label="egg">
-          🥚
-        </span>
-        El Huevo
-        <br />
-        <span role="img" aria-label="cherry">
-          🍒
-        </span>
-        La Cereza
-        <br />
-        <span role="img" aria-label="lemon">
-          🍋
-        </span>
-        El Limon
-        <br />
-        <span role="img" aria-label="oyster">
-          🦪
-        </span>
-        El Ostra
-        <br />
-        <span role="img" aria-label="coconut">
-          🥥
-        </span>
-        El Coco
-      </p>
+      <h1>This is a testing ground for functions to go into PlayComponent</h1>
+
+      <CurrentAnswer />
     </>
   );
 }
