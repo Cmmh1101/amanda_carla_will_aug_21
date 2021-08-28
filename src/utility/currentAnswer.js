41b08dc0576625a09361bb24a23c15e69a9895ac
illfriday <willprouty@gmail.com>
2021-08-27 22:07:29 -0700

    random emoji generation enabled!

--- src/utility/currentAnswer.js
+++ src/utility/currentAnswer.js
@@ -1,36 +1,47 @@
 import React, { Component } from "react";
 import { EMOJIS } from "../shared/emojis";
+import ShuffleArray from "./ShuffleArray";
 
 export default class CurrentAnswer extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      random: Math.floor(Math.random() * 30 + 1),
+      random: Math.floor(Math.random() * EMOJIS.length),
     };
+    this.generateAnswers = this.generateAnswers.bind(this);
+  }
+  //EMOJIS are randomized by utility/ShuffleArray.j
+  generateAnswers() {
+    const answers = [];
+    for (let i = 0; i < 4; i++) {
+      let num = Math.floor(Math.random() * EMOJIS.length);
+      if (answers.includes(num) || num === this.state.random) {
+        console.log("or cond Met", num);
+        i--;
+        continue;
+      } else {
+        answers.push(num);
+      }
+    }
+    answers.push(this.state.random);
+
+    console.log("OG", answers);
+    ShuffleArray(answers);
+    console.log("Shuf", answers);
+    return answers.map((i) => <>{EMOJIS[i].emoji} </>);
   }
 
-  // componentDidMount() {
-  //   this.setState(
-  //     {
-  //       random: Math.floor(Math.random() * 30 + 1),
-  //       // random: 3,
-  //     },
-  //     () => console.log(" setStatedidMount", this.state.random)
-  //   );
-  //   // console.log(this.state.random);
-  // }
-
   render() {
-    // const answer = () => EMOJIS.filter((em) => em.id === this.state.random)[0];
-    console.log("WHAT");
-    console.log("Render", this.state.random);
+    // console.log("Render", this.state.random);
+
     return (
       <>
-        <h3>El Emoji Correcto</h3>
-        <p>{EMOJIS[this.state.random].spanish}</p>
         <h3>La Palabra</h3>
+        <p>{EMOJIS[this.state.random].spanish}</p>
+        <h3>El Emoji Correcto</h3>
         <p>{EMOJIS[this.state.random].emoji}</p>
+        {this.generateAnswers()}
       </>
     );
   }
