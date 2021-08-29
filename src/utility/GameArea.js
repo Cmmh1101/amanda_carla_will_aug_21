6fc21087d3016b5f8cf8e367ef926be36458ed45
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-08-29 14:53:01 -0400

    Changing to functional components

--- src/utility/GameArea.js
+++ src/utility/GameArea.js
@@ -1,88 +1,65 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { EMOJIS } from "../shared/emojis";
 import ShuffleArray from "./ShuffleArray";
 import { Button } from "reactstrap";
 import { Fade } from "react-animation-components";
 
-export default class GameArea extends Component {
-  constructor(props) {
-    super(props);
+const refreshPage = () => {
+  window.location.reload(true);
+};
 
-    this.state = {
-      //INITIALIZING the random PROPERTY of the STATE as a RANDOM# from 1-30 RIGHT ANSWER
-      random: Math.floor(Math.random() * EMOJIS.length),
-      guessed: [],
-      score: 0,
-      remainingWords: 30,
-      lives: 5,
-      streak: 0
-    };
-    this.generateAnswers = this.generateAnswers.bind(this);
-    this.checkAnswer = this.checkAnswer.bind(this);
-    // this.getNewAnswer = this.getNewAnswer.bind(this);
-  }
+const GameArea = () => {
+  const [random, setRandom] = useState(0);
+  const [guessed, setGuessed] = useState([]);
+  const [score, setScore] = useState(0);
+  const [remainingWords, setRemainingWords] = useState(30);
+  const [lives, setLives] = useState(5);
+  const [streak, setStreak] = useState(0);
 
-  refreshPage = () => {
-    window.location.reload(true);
-  };
-
-  getNewAnswer = () => {
+  const getNewAnswer = () => {
     //REASSIGN NEW RANDOM#(MUST NOT BE IN guessed[])
     let num = Math.floor(Math.random() * EMOJIS.length);
-    if (this.state.guessed.includes(num)) {
-      this.getNewAnswer();
+    if (guessed.includes(num)) {
+      getNewAnswer();
     } else {
-      this.setState({
-        random: num,
-      });
+      setRandom(num)
     }
   };
 
-  checkAnswer = (event) => {
+  const checkAnswer = (event) => {
     // console.log(event);
     const clickedEmoji = event.target.innerText;
-    const correctEmoji = EMOJIS[this.state.random].emoji;
+    const correctEmoji = EMOJIS[random].emoji;
     console.log(clickedEmoji, correctEmoji);
     if (clickedEmoji === correctEmoji) {
       console.log("Correct");
       // let newScore = Number(this.state.score);
       // let newWordCount = Number(this.state.remainingWords);
-      this.setState({
-        score: (this.state.score + 20),
-        guessed: this.state.guessed.concat(this.state.random),
-        remainingWords: (this.state.remainingWords - 1),
-        streak: (this.state.streak + 1)
-      });
-      console.log("streak", this.state.streak)
-      if (this.state.streak >= 3) {
-        var streakPoints = 10 * this.state.streak
-        this.setState({
-          score: this.state.score + streakPoints
-        });
+      setScore(prevState => prevState + 20);
+      setGuessed(prevState => [...prevState, random]);
+      setRemainingWords(prevState => prevState - 1);
+      setStreak(prevState => prevState + 1);
+
+      console.log("streak", streak)
+      if (streak >= 3) {
+        const streakPoints = 10 * streak
+        setScore(prevState => prevState + streakPoints);
         console.log("Streak score", streakPoints);
       }
-      if (this.state.remainingWords === 0) {
+      if (remainingWords === 0) {
         alert(
-          `Congratulations! You Won. Your top score is ${this.state.score}.\nIf you are doing this for class, take a screenshot for your teacher and press "Restart" to start a new game.`
+          `Congratulations! You Won. Your top score is ${score}.\nIf you are doing this for class, take a screenshot for your teacher and press "Restart" to start a new game.`
         );
         // window.location.reload(true);
       }
-      this.getNewAnswer();
-      console.log(
-        "New Score",
-        this.state.score,
-        "\nGuessed:",
-        this.state.guessed
-      );
+      getNewAnswer();
     } else {
       // let newLives = Number(this.state.lives);
-      this.setState({
-        lives: (this.state.lives - 1),
-        streak: 0
-      });
-      if (this.state.lives > 0) {
+      setLives(prevState => prevState - 1);
+      setStreak(0);
+      if (lives > 0) {
         alert(
-          `You lost 1 Life.\nYou have ${this.state.lives} lives remaining.`
+          `You lost 1 Life.\nYou have ${lives} lives remaining.`
         );
       } else {
         alert("Game Over. Press 'Restart' to play again.");
@@ -90,17 +67,23 @@ export default class GameArea extends Component {
       }
     }
   };
+  console.log(
+    "New Score",
+    score,
+    "\nGuessed:",
+    guessed
+  );
   // console.log(clickedEmoji === correctEmoji ? "verdad" : "mierda");
 
   //EMOJIS are randomized by utility/ShuffleArray.js
-  generateAnswers = () => {
+  const GenerateAnswers = () => {
     const answers = [];
 
     //for() LOOP generates 4 X RANDOM #'S WRONG ANSWERS
 
     for (let i = 0; i < 4; i++) {
       let num = Math.floor(Math.random() * EMOJIS.length);
-      if (answers.includes(num) || num === this.state.random) {
+      if (answers.includes(num) || num === random) {
         // console.log("or cond Met", num);
         i--;
         continue;
@@ -108,92 +91,91 @@ export default class GameArea extends Component {
         answers.push(num);
       }
     }
-    answers.push(this.state.random);
+    answers.push(random);
 
     // console.log("OG", answers);
     ShuffleArray(answers);
     console.log("Shuf", answers);
 
-    if (this.state.remainingWords === 0 || this.state.lives === 0) {
+    if (remainingWords === 0 || lives === 0) {
       return <></>;
     } else {
       return answers.map((i) => (
-        <button className="emojis" key={i} onClick={this.checkAnswer}>
+        <button className="emojis" key={i} onClick={checkAnswer}>
           {EMOJIS[i].emoji}{" "}
         </button>
       ));
     }
   };
+  // console.log("Correcto", this.state.random);
 
-  render() {
-    // console.log("Correcto", this.state.random);
+  return (
+    <>
+      <div className="col-md-8 game-area">
+        <div className="play_restart_btns">
+          <Button className="btn-block" onClick={refreshPage}>
+            Restart
+          </Button>
+        </div>
+        <div className="board">
+          <div className="col-4 board_item">
+            <h6>Points</h6>
+            <hr />
+            <span>{score}</span>
+          </div>
+          <div className="col-4 board_item">
+            <h6>Words</h6>
+            <hr />
+            <span>{remainingWords}</span>
+          </div>
+          <div className="col-4 board_item">
+            <h6>Lives</h6>
+            <hr />
+            <span>{lives}</span>
+          </div>
+        </div>
+        {/* Game Execussion */}
 
-    return (
-      <>
-        <div className="col-md-8 game-area">
-          <div className="play_restart_btns">
-            <Button className="btn-block" onClick={this.refreshPage}>
-              Restart
-            </Button>
-          </div>
-          <div className="board">
-            <div className="col-4 board_item">
-              <h6>Points</h6>
-              <hr />
-              <span>{this.state.score}</span>
-            </div>
-            <div className="col-4 board_item">
-              <h6>Words</h6>
-              <hr />
-              <span>{this.state.remainingWords}</span>
-            </div>
-            <div className="col-4 board_item">
-              <h6>Lives</h6>
-              <hr />
-              <span>{this.state.lives}</span>
-            </div>
-          </div>
-          {/* Game Execussion */}
-
-          <div className="play">
-            <Fade in>
-              <p className="word_in_play active">
-                {EMOJIS[this.state.random].spanish}
-                <img
-                  className="word-jar"
-                  src="../images/glassjar.png"
-                  alt="jar"
-                />
-              </p>
-            </Fade>
-          </div>
-
-          <div className="answer_options">
-            <h3>Los Opciones</h3>
-            {this.generateAnswers()}
-          </div>
-          <img
-            className="game_area_img"
-            src="../images/basekitchen.png"
-            alt=""
-          />
+        <div className="play">
+          <Fade in>
+            <p className="word_in_play active">
+              {EMOJIS[random].spanish}
+              <img
+                className="word-jar"
+                src="../images/glassjar.png"
+                alt="jar"
+              />
+            </p>
+          </Fade>
         </div>
 
-        {/* <button onClick={this.refreshPage}>Restart</button> */}
-        {/* <h3>Puntaje</h3> */}
-        {/* <p>{this.state.score}</p> */}
-        {/* <h3>Las Palabras Restante</h3> */}
-        {/* <p>{this.state.remainingWords}</p> */}
-        {/* <h3>Las Vidas</h3> */}
-        {/* <p>{this.state.lives}</p> */}
-        {/* <h3>La Palabra</h3> */}
-        {/* <p>{EMOJIS[this.state.random].spanish}</p> */}
-        {/* <h3>El Emoji Correcto</h3>
+        <div className="answer_options">
+          <h3>Los Opciones</h3>
+          <GenerateAnswers />
+        </div>
+        <img
+          className="game_area_img"
+          src="../images/basekitchen.png"
+          alt=""
+        />
+      </div>
+
+      {/* <button onClick={this.refreshPage}>Restart</button> */}
+      {/* <h3>Puntaje</h3> */}
+      {/* <p>{this.state.score}</p> */}
+      {/* <h3>Las Palabras Restante</h3> */}
+      {/* <p>{this.state.remainingWords}</p> */}
+      {/* <h3>Las Vidas</h3> */}
+      {/* <p>{this.state.lives}</p> */}
+      {/* <h3>La Palabra</h3> */}
+      {/* <p>{EMOJIS[this.state.random].spanish}</p> */}
+      {/* <h3>El Emoji Correcto</h3>
         <p>{EMOJIS[this.state.random].emoji}</p> */}
 
-        {/* <h3>Los Opciones</h3> */}
-        {/* {this.generateAnswers()} */}
-      </>
-    );
-  }
+      {/* <h3>Los Opciones</h3> */}
+      {/* {this.generateAnswers()} */}
+    </>
+  );
 }
+
+export default GameArea
