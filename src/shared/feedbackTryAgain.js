86ab642fe61f5d4b67817d84882f5615e3bec782
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-09-01 12:39:15 -0400

    starting neg feedback

---
+++ src/shared/feedbackTryAgain.js
@@ -0,0 +1,70 @@
+export const FEEDBACKTRYAGAIN = [
+    {
+        id: 1,
+        feedback: "Try again",
+    },
+    {
+        id: 2,
+        feedback: "Almost",
+    },
+    {
+        id: 3,
+        feedback: "Not quite...",
+    },
+    {
+        id: 4,
+        feedback: "",
+    },
+    {
+        id: 5,
+        feedback: "",
+    },
+    {
+        id: 6,
+        feedback: "",
+    },
+    {
+        id: 7,
+        feedback: "",
+    },
+    {
+        id: 8,
+        feedback: "",
+    },
+    {
+        id: 9,
+        feedback: "",
+    },
+    {
+        id: 10,
+        feedback: "",
+    },
+    {
+        id: 11,
+        feedback: "",
+    },
+    {
+        id: 12,
+        feedback: "",
+    },
+    {
+        id: 13,
+        feedback: "",
+    },
+    {
+        id: 14,
+        feedback: "",
+    },
+    {
+        id: 15,
+        feedback: "",
+    },
+    {
+        id: 16,
+        feedback: "",
+    },
+    {
+        id: 17,
+        feedback: "",
+    },
+];
